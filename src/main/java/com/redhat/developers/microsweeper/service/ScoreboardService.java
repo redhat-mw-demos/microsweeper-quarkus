b87293273c0ce6c4dde0def2020fef93eb004b14
Daniel Oh <doh@redhat.com>
2024-02-17 00:22:16 -0500

    Remove Micrometer and Add Otel

--- src/main/java/com/redhat/developers/microsweeper/service/ScoreboardService.java
+++ src/main/java/com/redhat/developers/microsweeper/service/ScoreboardService.java
@@ -2,28 +2,28 @@ package com.redhat.developers.microsweeper.service;
 
 import com.redhat.developers.microsweeper.model.Score;
 
-import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.Timer;
+// import io.micrometer.core.annotation.Timed;
+// import io.micrometer.core.instrument.Metrics;
+// import io.micrometer.core.instrument.Timer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 @ApplicationScoped
-public class ScoreboardService  {
+public class ScoreboardService {
 
     public List<Score> getScoreboard() {
         return Score.listAll();
     }
 
     @Transactional
-    @Timed(description = "How log to add a score", value = "scoreboard-timer")
+    // @Timed(description = "How log to add a score", value = "scoreboard-timer")
     public void addScore(Score score) {
-        Timer timer = Metrics.globalRegistry.timer("example.prime.number.test");
-        timer.record(() -> {
-            Score.persist(score);
-        });
+        // Timer timer = Metrics.globalRegistry.timer("example.prime.number.test");
+        // timer.record(() -> {
+        Score.persist(score);
+        // });
     }
 
     @Transactional
